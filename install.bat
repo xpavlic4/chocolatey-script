e16b77dad9dc22615168550b17255f496271cf4a
Radim Pavlicek <radim.pavlicek@gmail.com>
2018-10-30 14:16:45 +0100

    init commit

---
+++ install.bat
@@ -0,0 +1,14 @@
+@echo off
+
+choco install googlechrome -y
+choco install firefox -y
+choco install vlc -y
+choco install git.install -y
+choco install jdk8 -y
+choco install atom -y
+choco install sbt -y
+choco install adb -y
+choco install scala -y
+choco install treesizefree -y
+choco install itunes -y
+choco install awscli -y
